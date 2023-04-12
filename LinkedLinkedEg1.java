22f313b4e05063e4516e9f2925684847385aa5b3
PrinceKumar19990 <122788054+PrinceKumar19990@users.noreply.github.com>
2023-04-12 22:44:46 +0530

    Add files via upload

---
+++ LinkedLinkedEg1.java
@@ -0,0 +1,10 @@
+package Collection;
+
+public class LinkedLinkedEg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ vectorEg1.java
@@ -0,0 +1,10 @@
+package Collection;
+
+public class vectorEg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
