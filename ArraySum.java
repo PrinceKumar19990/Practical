66c5c9ca751244727f85e8baf1d07c3209a5d03c
PrinceKumar19990 <122788054+PrinceKumar19990@users.noreply.github.com>
2023-04-23 23:58:54 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,12 @@
+package ArrayEG;
+
+public class ArraySum {
+	public static void main(String[] args) {
+	int sum = 0;
+	 int Num_array[] = {1,2,4,6,4};
+	 for (int i:Num_array) {
+     sum +=i;
+     System.out.println(" The sum is "+sum);
+	}
+
+}}
